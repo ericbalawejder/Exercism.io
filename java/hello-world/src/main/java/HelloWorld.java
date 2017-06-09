6b11178933aa8a02b777b5df3e291469bc79212f
Eric Balawejder <eric.balawejder@gmail.com>
2017-06-08 22:53:08 -0400

    defined tab to spaces

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld 
 {
-	public static String hello(String name) 
-	{
-		if(name == null || "".equals(name)) 
-		{
-			name = "World";
-		}
-		return "Hello, " + name + "!";
-	}
+    public static String hello(String name) 
+    {
+        if(name == null || "".equals(name)) 
+        {
+            name = "World";
+        }
+        return "Hello, " + name + "!";
+    }
 }
