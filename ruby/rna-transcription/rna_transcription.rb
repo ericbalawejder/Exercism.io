9f6a276e12ff2095079e3ef22522015d9f303fc7
Eric Balawejder <eric.balawejder@gmail.com>
2017-06-19 23:25:53 -0400

    rubocop approved

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,20 +1,18 @@
+# Given a DNA strand, return its RNA complement (per RNA transcription).
 class Complement
-  COMPLEMENTS = {'G' => 'C',
-                 'C' => 'G',
-                 'T' => 'A',
-                 'A' => 'U'}.freeze
+  COMPLEMENTS = { 'G' => 'C',
+                  'C' => 'G',
+                  'T' => 'A',
+                  'A' => 'U' }.freeze
   # will not work
-  #COMPLEMENTS.default = "*" 
+  # COMPLEMENTS.default = "*"
 
   def self.of_dna(strand)
     # COMPLEMENTS.fetch(nucleotide, "default_value for nil key")
-    rna = strand.chars.map {|nucleotide| COMPLEMENTS.fetch(nucleotide, "*")}.join
-    # if it contains the default value, assign it to the empty string 
-    if rna.include?("*")
-      rna = ""
-    end
-    
-    return rna
+    rna = strand.chars.map { |nucleotide| COMPLEMENTS.fetch(nucleotide, '*') }.join
+    # if rna contains the default value, make rna the empty string
+    rna = '' if rna.include?('*')
+    rna
   end
 end
 
