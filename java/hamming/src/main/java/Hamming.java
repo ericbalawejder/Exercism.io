c339af9cc6da707a32b99967293d5ea57ee9e7cc
Eric Balawejder <eric.balawejder@gmail.com>
2017-07-11 14:19:51 -0400

    new language go, java hamming

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,35 @@
+// Calculate the Hamming difference between two DNA strands.
+public class Hamming
+{
+    private String leftStrand;
+    private String rightStrand;
+    
+    Hamming(String leftStrand, String rightStrand)
+    {
+        if (leftStrand.length() != rightStrand.length())
+        {
+            throw new IllegalArgumentException();
+        }
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+    }
+
+    int getHammingDistance()
+    {
+        int distance = 0;
+        for (int i = 0; i < leftStrand.length(); i++)
+        {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i))
+            {
+                distance++;
+            }
+        }
+        return distance;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Deez Nuts");
+        Hamming hamming = new Hamming("ACT", "GAT");
+        System.out.println(hamming.getHammingDistance());
+    }
+}
