49a6390c91b809b45240223a178c981a046b3d55
Eric Balawejder <eric.balawejder@gmail.com>
2017-08-15 22:28:43 -0400

    added enums class for constants

--- java/space-age/src/main/java/SpaceAge.java
+++ java/space-age/src/main/java/SpaceAge.java
@@ -1,9 +1,31 @@
-// Given an age in seconds, calculate how old someone would be on:
-class SpaceAge
+// Given an seconds in seconds, calculate how old someone would be on:
+public class SpaceAge
 {
-    private final double seconds;
-    private static final double secondsInEarthYear = 31557600;
-    
+    private enum ORBITAL_PERIOD
+    {
+        SECONDS_IN_EARTH_YEAR(31557600),
+        // Orbital period values represented in Earth years.
+        MERCURY(0.2408467), 
+        VENUS(0.61519726),
+        MARS(1.8808158),
+        JUPITER(11.862615),
+        SATURN(29.447498),
+        URANUS(84.016846),
+        NEPTUNE(164.79132);
+
+        private final double earthYears;
+        ORBITAL_PERIOD(double earthYears)
+        {
+            this.earthYears = earthYears;
+        }
+        public double getOrbit()
+        {
+            return earthYears;
+        }
+    }
+
+    private final Double seconds;
+
     SpaceAge(double seconds)
     {
         this.seconds = seconds;
@@ -16,41 +38,41 @@ class SpaceAge
 
     double onEarth()
     {
-        return getSeconds() / secondsInEarthYear;
+        return getSeconds() / ORBITAL_PERIOD.SECONDS_IN_EARTH_YEAR.getOrbit();
     }
-    
+
     double onMercury()
     {
-        return onEarth() / 0.2408467;
+        return onEarth() / ORBITAL_PERIOD.MERCURY.getOrbit();
     }
 
     double onVenus()
     {
-        return onEarth() / 0.61519726;
+        return onEarth() / ORBITAL_PERIOD.VENUS.getOrbit();
     }
 
     double onMars()
     {
-        return onEarth() / 1.8808158;
+        return onEarth() / ORBITAL_PERIOD.MARS.getOrbit();
     }
 
     double onJupiter()
     {
-        return onEarth() / 11.862615;
-    }
+        return onEarth() / ORBITAL_PERIOD.JUPITER.getOrbit();
+    }	
 
     double onSaturn()
     {
-        return onEarth() / 29.447498;
+        return onEarth() / ORBITAL_PERIOD.SATURN.getOrbit();
     }
 
     double onUranus()
     {
-        return onEarth() / 84.016846;
+        return onEarth() / ORBITAL_PERIOD.URANUS.getOrbit();
     }
 
     double onNeptune()
     {
-        return onEarth() / 164.79132;
+        return onEarth() / ORBITAL_PERIOD.NEPTUNE.getOrbit();
     }
 }
