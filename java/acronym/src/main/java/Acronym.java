8e3869305a00b25f13469a8dfb9451850e029912
Eric Balawejder <eric.balawejder@gmail.com>
2017-11-10 01:20:59 -0500

    return statement

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -13,7 +13,6 @@ class Acronym
         // https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
         // Removes every character which is not on a word boundary on it's left.
         // Removes any character which is not a letter, including spaces.
-        String result = phrase.replaceAll("\\B.|\\P{L}", "").toUpperCase();
-        return result;
+        return phrase.replaceAll("\\B.|\\P{L}", "").toUpperCase();
     }
 }
