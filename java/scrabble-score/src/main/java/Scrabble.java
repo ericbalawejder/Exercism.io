6d463fc3294eb08c4becdd5eedfa014d02cc45d9
Eric Balawejder <eric.balawejder@gmail.com>
2017-11-15 23:28:50 -0500

    refactored scrabble score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,10 +1,24 @@
 // Given a word, compute the scrabble score for that word.
+import java.util.Map;
 import java.util.HashMap;
 
 class Scrabble
 {
+    private static final Map<String, Integer> characterMap = new HashMap<>();
+
+    static
+    {
+        characterMap.put("AEIOULNRST", 1);
+        characterMap.put("DG", 2);
+        characterMap.put("BCMP", 3);
+        characterMap.put("FHVWY", 4);
+        characterMap.put("K", 5);
+        characterMap.put("JX", 8);
+        characterMap.put("QZ", 10);
+    }
+
     private String word;
-    
+
     Scrabble(String word)
     {
         this.word = word;
@@ -12,43 +26,17 @@ class Scrabble
 
     int getScore()
     {
-        HashMap<String, Integer> characterValueHash = new HashMap<>();
-        String[] wordArray = word.toUpperCase().split("");
-        characterValueHash.put("", 0);
-        characterValueHash.put("A", 1);
-        characterValueHash.put("E", 1);
-        characterValueHash.put("I", 1);
-        characterValueHash.put("O", 1);
-        characterValueHash.put("U", 1);
-        characterValueHash.put("L", 1);
-        characterValueHash.put("N", 1);
-        characterValueHash.put("R", 1);
-        characterValueHash.put("S", 1);
-        characterValueHash.put("T", 1);
-        characterValueHash.put("D", 2);
-        characterValueHash.put("G", 2);
-        characterValueHash.put("B", 3);
-        characterValueHash.put("C", 3);
-        characterValueHash.put("M", 3);
-        characterValueHash.put("P", 3);
-        characterValueHash.put("F", 4);
-        characterValueHash.put("H", 4);
-        characterValueHash.put("V", 4);
-        characterValueHash.put("W", 4);
-        characterValueHash.put("Y", 4);
-        characterValueHash.put("K", 5);
-        characterValueHash.put("J", 8);
-        characterValueHash.put("X", 8);
-        characterValueHash.put("Q", 10);
-        characterValueHash.put("Z", 10);
-                
         int scrabbleScore = 0;
-        for (String character : wordArray)
+        for (int i = 0; i < word.length(); i++)
         {
-            if (characterValueHash.containsKey(character))
+            String letter = String.valueOf(word.charAt(i));
+            // Make characterMap a set and iterate through each key(letter).
+            for (Map.Entry<String, Integer> characterMapEntry : characterMap.entrySet())
             {
-                int characterValue = characterValueHash.get(character);
-                scrabbleScore += characterValue;
+                if (characterMapEntry.getKey().contains(letter.toUpperCase()))
+                {
+                    scrabbleScore += characterMapEntry.getValue();
+                }
             }
         }
         return scrabbleScore;
