aaa0aab02617257fb5e69731c18efcb115ee514c
Eric Balawejder <eric.balawejder@gmail.com>
2018-01-01 23:12:45 -0500

    gigasecond

--- bash/gigasecond/gigasecond.sh
+++ bash/gigasecond/gigasecond.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# -x: all executed commands are printed to the terminal.
-#set -x
-# -e exits whenever anything returns a non-zero value.
-# -u makes it an error to reference a non-existent environment variable
-set -eu
-# raises error for misspelled commands
-set -o pipefail
-
-
 #if there are no arguments, exit
 if [ $# -eq 0 ]
 then
@@ -19,4 +10,4 @@ fi
 start_date=$1
 gigasecond=1000000000
 date_to_seconds=$(date +%s --date="$start_date")
-date --date="@$((date_to_seconds + $gigasecond))"
+date --date="@$((date_to_seconds + $gigasecond))" +"%a %b %-e %T %Z %Y"
