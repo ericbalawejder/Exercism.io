c9b4003bb206c39f7cf2c8508bdc9d41bfe5b140
Eric Balawejder <eric.balawejder@gmail.com>
2018-01-17 10:54:12 -0500

    initial proverb.java

---
+++ java/proverb/src/main/java/Proverb.java
@@ -0,0 +1,43 @@
+class Proverb
+{
+    public static void main(String[] args)
+    {
+        String[] poem  = new String[]{"nail", "shoe", "horse", "rider", "message", "battle", "kingdom"};
+        Proverb proverb = new Proverb(poem);
+        System.out.println(proverb.recite());
+    }
+    
+    private final String[] words;
+
+    Proverb(String[] words)
+    {
+        this.words = words;
+    }
+
+    String recite()
+    {
+        StringBuilder poem = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--)
+        {
+            if (words.length == 1)
+            {
+                String endLine = "And all for the want of a " + words[i] + ".";
+                poem.append(endLine);
+                break;
+            }
+            else
+            {
+                if (i == 0)
+                {
+                    String endLine = "And all for the want of a " + words[i] + ".";
+                    poem.append(endLine);
+                    break;
+                }
+                String line = "For want of " + words[i - 1] + " the " + words[i] + " was lost.\n";
+                poem.append(line);
+            }
+        }
+        return poem.toString();
+    }
+}
