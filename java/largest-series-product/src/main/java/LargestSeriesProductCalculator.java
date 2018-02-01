8c0feb2153881a11e11a7cb9aec999ebc6d2614f
Eric Balawejder <eric.balawejder@gmail.com>
2018-02-01 00:39:32 -0500

    LargestSeriesProductCalculator refactored

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,56 +1,52 @@
+// Given a string of digits, calculate the largest product for a contiguous
+// substring of digits of length n.
 class LargestSeriesProductCalculator
 {
-    public static void main(String[] args)
-    {
-        LargestSeriesProductCalculator series = new LargestSeriesProductCalculator("9987x99");
-        System.out.println(series.calculateLargestProductForSeriesLength(3));
-    }
-    
     private final String inputNumber;
     
     LargestSeriesProductCalculator(String inputNumber)
     {
+        // if inputNumber does not contain zero or more digits only.
         if (!inputNumber.matches("\\d*"))
         {
-            throw new IllegalArgumentException("String to search may only contain digits.");
+            throw new IllegalArgumentException(
+                "String to search may only contain digits.");
         }
-        this.inputNumber = inputNumber;
+        else
+        {
+            this.inputNumber = inputNumber;
+        }
     }
 
-    long calculateLargestProductForSeriesLength(int numberOfDigits) throws IllegalArgumentException
+    long calculateLargestProductForSeriesLength(int numberOfDigits)
     {
         long largestProduct = 0;
         if (numberOfDigits > inputNumber.length())
         {
             throw new IllegalArgumentException(
-                "Series length must be less than or equal to the length of the string to search.");
+                "Series length must be less than or equal to the length " + 
+                    "of the string to search.");
         }
         else if (numberOfDigits < 0)
         {
-            throw new IllegalArgumentException("Series length must be non-negative.");
+            throw new IllegalArgumentException(
+                "Series length must be non-negative.");
         }
         else
         {
-            try
+            int numberOfSubstrings = inputNumber.length() - numberOfDigits + 1;
+            for (int i = 0; i < numberOfSubstrings; i++)
             {
-                int numberOfSubstrings = inputNumber.length() - numberOfDigits + 1;
-                for (int i = 0; i < numberOfSubstrings; i++)
+                String substring = inputNumber.substring(i, i + numberOfDigits);
+                long product = 1;
+                for (int j = 0; j < numberOfDigits; j++)
                 {
-                    String substring = inputNumber.substring(i, i + numberOfDigits);
-                    long product = 1;
-                    for (int j = 0; j < numberOfDigits; j++)
-                    {
-                        product *= Long.parseLong(substring.substring(j, j + 1));
-                    }
-                    if (product > largestProduct)
-                    {
-                        largestProduct = product;
-                    }
+                    product *= Long.parseLong(substring.substring(j, j + 1));
                 }
-            }
-            catch (NumberFormatException numberFormatException)
-            {
-                throw new IllegalArgumentException("String to search may only contain digits.");
+                if (product > largestProduct)
+                {
+                    largestProduct = product;
+                }
             }
         }
         return largestProduct;
