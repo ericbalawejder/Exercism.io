a1b332826e3dd8426ce9a71dc2b2e1efb63780be
Eric Balawejder <eric.balawejder@gmail.com>
2018-02-26 23:27:58 -0500

    refactored rotational cipher

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -15,12 +15,12 @@ class RotationalCipher
         {
             if (Character.isUpperCase(data.charAt(i)))
             {
-                char character = (char)(((int)data.charAt(i) + shiftKey - 65) % 26 + 65);
+                char character = (char)(((int)data.charAt(i) + shiftKey - (int)'A') % 26 + (int)'A');
                 cipher.append(character);
             }
             else if (Character.isLowerCase(data.charAt(i)))
             {
-                char character = (char)(((int)data.charAt(i) + shiftKey - 97) % 26 + 97);
+                char character = (char)(((int)data.charAt(i) + shiftKey - (int)'a') % 26 + (int)'a');
                 cipher.append(character);
             }
             else
