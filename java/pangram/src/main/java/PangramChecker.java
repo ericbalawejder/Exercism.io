4f245c6dbeec70fe660b34f770c11c596c32814f
Eric Balawejder <eric.balawejder@gmail.com>
2018-07-05 18:38:41 -0400

    added int cast to pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -3,21 +3,18 @@ import java.util.Set;
 import java.util.HashSet;
 import java.lang.Character;
 
-public class PangramChecker 
-{
-    public boolean isPangram(String input) 
-    {
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
         String downcased = input.toLowerCase();
-        Set<Character> set = new HashSet<Character>();
+        Set<Character> set = new HashSet<>();
         char[] characterArray = downcased.toCharArray();
-        for (char character : characterArray)
-        {
+        for (char character : characterArray) {
             // Ascii code [a-z]
-            if (97 <= character && character <= 122)
-            {
+            if ((int)'a' <= character && character <= (int)'z') {
                 set.add(character);
             }
-        }        
+        }
         return set.size() == 26;
     }
 }
