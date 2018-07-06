afe95b7f0214a8678e953b34db1846dea075e8b1
Eric Balawejder <eric.balawejder@gmail.com>
2018-07-06 15:44:47 -0400

    pangram lambdas wip

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,20 +1,41 @@
 // Determine if a sentence is a pangram.
-import java.util.Set;
 import java.util.HashSet;
-import java.lang.Character;
+import java.util.Set;
+
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class PangramChecker {
 
-    public boolean isPangram(String input) {
-        String downcased = input.toLowerCase();
-        Set<Character> set = new HashSet<>();
-        char[] characterArray = downcased.toCharArray();
-        for (char character : characterArray) {
-            // Ascii code [a-z]
-            if ((int)'a' <= character && character <= (int)'z') {
-                set.add(character);
-            }
-        }
-        return set.size() == 26;
-    }
+  public static void main(String... args) {
+    String testTrue = "the_quick_brown_fox_jumps_over_the_lazy_dog";
+    String testFalse = "a quick movement of the enemy will jeopardize five gunboats";
+    String testWithNumbersTrue = "the 1 quick brown foX Jumps over the 2 lazy doGs";
+
+    System.out.println(isPangram(testWithNumbersTrue));
+    System.out.println(isPangram(testWithNumbersTrue));
+    System.out.println(isPangram(testWithNumbersTrue));
+
+    Stream<Character> characterStream = testFalse.chars()
+      .mapToObj(c -> (char) c);
+    characterStream.forEach(System.out::print);
+    
+    System.out.println();
+  }
+
+  public static boolean isPangram(String input) {
+    Set<Integer> set = new HashSet<>();
+
+    input
+        .toLowerCase()
+        .chars()
+        .filter(character -> Character.isAlphabetic(character))
+        .forEach(
+            e -> {
+              if (!set.contains(e)) {
+                set.add(e);
+              }
+            });
+    return set.size() == 26;
+  }
 }
