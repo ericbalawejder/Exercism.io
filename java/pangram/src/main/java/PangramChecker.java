2d19f457841c6683ea4fc53c02d14f2abf19696e
Eric Balawejder <eric.balawejder@gmail.com>
2018-07-08 21:48:09 -0400

    pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -4,13 +4,6 @@ import java.util.HashSet;
 
 public class PangramChecker {
 
-  public static void main(String... args) {
-    String testString = "Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.";
-    
-    System.out.println(isPangram(testString));
-    System.out.println(isPangram3(testString));
-  }
-
   public static boolean isPangram(String input) {
     Set<Integer> set = new HashSet<>();
 
@@ -26,14 +19,4 @@ public class PangramChecker {
             });
     return set.size() == 26;
   }
-  
-  public static boolean isPangram3(String input) {
-    return input
-        .toLowerCase()
-        .chars()
-        //.filter(Character::isAlphabetic)
-        .filter(character -> (int)'a' <= character && character <= (int)'z')
-        .distinct()
-        .count() == 26;
-      }
 }
