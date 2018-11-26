c7f0d01d2322bf1521b387ee50410b0d53d0029a
Eric Balawejder <eric.balawejder@gmail.com>
2018-11-25 19:19:45 -0500

    completed Euclid's algorithm

--- java/pythagorean-triplet/src/main/java/PythagoreanTripletBuilder.java
+++ java/pythagorean-triplet/src/main/java/PythagoreanTripletBuilder.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 class PythagoreanTripletBuilder {
     int lower = 1;
     int upper;
-    //int sum;
-    // int sum = null;?
     Integer sum;
 
     PythagoreanTripletBuilder withFactorsGreaterThanOrEqualTo(int lower) {
@@ -37,13 +35,17 @@ class PythagoreanTripletBuilder {
         for (int n = 1; n <= Math.sqrt(upper); n++) {
             for (int m = n + 1; m <= Math.sqrt(upper); m++) {
                 if (gcd(m, n) == 1 && !(m % 2 != 0 && n % 2 != 0)) {
-                    int a = ((int) (Math.pow(m, 2) - Math.pow(n, 2)));
-                    int b = 2 * m * n;
-                    int c = ((int) (Math.pow(m, 2) + Math.pow(n, 2)));
-                
-                    PythagoreanTriplet triplet = new PythagoreanTriplet(a, b, c);
-                    if (triplet.isPythagorean() && a >= lower && b >= lower && c <= upper && (sum == null || triplet.calculateSum() == sum)) {
-                        results.add(triplet);
+
+                    for (int k = 1; k <= upper / 5; k++) {
+                        int a = k * ((int) (Math.pow(m, 2) - Math.pow(n, 2)));
+                        int b = k * 2 * m * n;
+                        int c = k * ((int) (Math.pow(m, 2) + Math.pow(n, 2)));
+
+                        PythagoreanTriplet triplet = new PythagoreanTriplet(a, b, c);
+                        if (a >= lower && b >= lower && c <= upper
+                            && (sum == null || triplet.calculateSum() == sum)) {
+                                results.add(triplet);
+                        }
                     }
                 }
             }
