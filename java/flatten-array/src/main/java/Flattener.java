33beb3d0a24600b8db3e2ca88b38c39860a8199a
Eric Balawejder <eric.balawejder@gmail.com>
2018-11-26 18:59:05 -0500

    second iteration of flatten

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -1,6 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
@@ -14,37 +17,45 @@ public class Flattener {
                                         100, 
                                         "four",
                                         singletonList(singletonList((singletonList(50)))), "-2"));
-        
+
         List<List<?>> nestedList = asList(
                         asList("one"),
                         asList(3, 4),
                         singletonList(singletonList(singletonList(2))),
                         asList(asList("two", 'd', "5656")),
                         asList("three", "three:two", "three:three", "three:four"));
-        
-        System.out.println(nestedList);
-        
+
+        List<Object> treeList = a(a(1), 2, a(a(3, 4), 5), a(a(a())), a(a(a(6))), 7, 8, a());
+
         Flattener flattener = new Flattener();
+
+        System.out.println(treeList);
+        System.out.println(flattener.flatten(treeList));
+
         System.out.println(list);
         System.out.println(flattener.flatten(list));
+
+        System.out.println(nestedList);
+        System.out.println(flattener.flatten(nestedList));
     }
 
-    List<Object> flatten(List<Object> nestedList) {
-        List<Object> flatList = new ArrayList<Object>();
-        search(flatList, nestedList);
+    List<Object> flatten(List<?> nestedList) {
+        List<Object> flatList = new ArrayList<>();
+        flatten(nestedList, flatList);
         return flatList;
     }
 
-    @SuppressWarnings("unchecked")
-    private void search(List<Object> flatList, List<Object> nestedList) {
+    private void flatten(List<?> nestedList, List<Object> flatList) {
         for (Object element : nestedList) {
-            if (element == null) {
-                continue;
-            } else if (element instanceof List) {
-                search(flatList, (List<Object>) element);
-            } else {
+            if (element instanceof List<?>) {
+                flatten((List<?>) element, flatList);
+            } else if (element != null) {
                 flatList.add(element);
             }
         }
+    }
+
+    private static List<Object> a(Object... a) {
+        return asList(a);
     }
 }
