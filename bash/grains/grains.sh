0fc13721baa285c106012e349e33b9bedc846058
Eric Balawejder <eric.balawejder@gmail.com>
2019-01-23 22:26:37 -0500

    refactored grains.sh

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -9,12 +9,9 @@ fi
 case "$1" in
   [1-9]|[1-5][0-9]|6[0-4])
     printf "%u\n" $((1 << $1 - 1))
-    #echo 2^$(($1-1)) | bc
     ;;
   total)
     printf "%u\n" $((2**64-1))
-    #printf "%llu\n" $((((1 << 64)) - 1))
-    #echo 2^64 - 1 | bc
     ;;
   *)
     echo "Error: invalid input"
