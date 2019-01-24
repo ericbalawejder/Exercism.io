5dc16bc055e79c0f180d613edb530e8a6c6ef525
Eric Balawejder <eric.balawejder@gmail.com>
2019-01-23 21:34:01 -0500

    refactored Hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,29 +1,22 @@
-// Calculate the Hamming difference between two DNA strands.
-public class Hamming
-{
+import java.util.stream.IntStream;
+
+public class Hamming {
     private String leftStrand;
     private String rightStrand;
-    
-    Hamming(String leftStrand, String rightStrand)
-    {
-        if (leftStrand.length() != rightStrand.length())
-        {
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
             throw new IllegalArgumentException();
         }
         this.leftStrand = leftStrand;
         this.rightStrand = rightStrand;
     }
 
-    int getHammingDistance()
-    {
-        int distance = 0;
-        for (int i = 0; i < leftStrand.length(); i++)
-        {
-            if (leftStrand.charAt(i) != rightStrand.charAt(i))
-            {
-                distance++;
-            }
-        }
-        return distance;
+    int getHammingDistance() {
+        return IntStream
+                .range(0, leftStrand.length())
+                .map(i -> leftStrand.charAt(i) != rightStrand.charAt(i) ? 1 : 0)
+                .sum();
     }
+
 }
