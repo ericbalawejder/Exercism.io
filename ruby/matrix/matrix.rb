502c13f2f361234c386ff3c192449fad2bfd49ad
Eric Balawejder <eric.balawejder@gmail.com>
2019-03-13 22:10:03 -0400

    refactored ruby matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,7 +2,13 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(matrix)
-    @rows = matrix.split("\n").map { |row| row.split(' ').map(&:to_i) }
+    @rows = create_rows(matrix)
     @columns = rows.transpose
+  end
+
+  private
+
+  def create_rows(matrix)
+    matrix.each_line.map { |row| row.split.map(&:to_i) }
   end
 end
