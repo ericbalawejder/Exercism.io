15f8b34a19c6d423c1c2e0be6cf2a0cb6bb99e65
Eric Balawejder <eric.balawejder@gmail.com>
2019-04-22 22:43:56 -0400

    added generic implementation to binary search

--- java/binary-search/src/main/java/BinarySearch.java
+++ java/binary-search/src/main/java/BinarySearch.java
@@ -1,57 +1,30 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-class BinarySearch {
+class BinarySearch<T> {
 
-    public static void main(String[] args) throws ValueNotFoundException {
-        List<Integer> sortedList = Collections.unmodifiableList(Arrays.asList(1, 3, 4, 6, 8, 9, 11));
+    private final List<T> sortedList;
 
-        BinarySearch searchDeezNutz = new BinarySearch(sortedList);
-
-        System.out.println(searchDeezNutz.indexOf(11));
+    BinarySearch(final List<T> sortedList) {
+        this.sortedList = Collections.unmodifiableList(new ArrayList<T>(sortedList));
     }
 
-    private final List<Integer> sortedList;
-
-    BinarySearch(final List<Integer> sortedList) {
-        this.sortedList = Collections.unmodifiableList(new ArrayList<Integer>(sortedList));
-    }
-
-    int indexOf1(int target) throws ValueNotFoundException {
-        int high = sortedList.size() - 1;
-        int low = 0;
-
-        while (high >= low) {
-            int mid = (high + low) / 2;
-
-            if (target == sortedList.get(mid)) {
-                return mid;
-            } else if (target < sortedList.get(mid)) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        throw new ValueNotFoundException("Value not in array");
-    }
-
-    int indexOf(int target) throws ValueNotFoundException {
+    int indexOf(Comparable<T> target) throws ValueNotFoundException {
         int low = 0;
         int high = sortedList.size() - 1;
         return indexOf(target, low, high);
     }
 
-    private int indexOf(int target, int low, int high) throws ValueNotFoundException {
+    private int indexOf(Comparable<T> target, int low, int high) throws ValueNotFoundException {
         if (low > high) {
             throw new ValueNotFoundException("Value not in array");
         }
         int mid = (low + high) / 2;
 
-        if (target == sortedList.get(mid)) {
+        if (target.compareTo(sortedList.get(mid)) == 0) {
             return mid;
-        } else if (target < sortedList.get(mid)) {
+        } else if (target.compareTo(sortedList.get(mid)) < 0) {
             return indexOf(target, low, mid - 1);
         } else {
             return indexOf(target, mid + 1, high);
