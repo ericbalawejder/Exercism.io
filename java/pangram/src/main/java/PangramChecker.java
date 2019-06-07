1355ceae9aac8ac63166a6a934ec95af8d947751
Eric Balawejder <eric.balawejder@gmail.com>
2019-06-07 11:19:09 -0400

    refactored java pangram for updated tests

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,22 +1,19 @@
-// Determine if a sentence is a pangram.
 import java.util.Set;
 import java.util.HashSet;
 
 public class PangramChecker {
 
-  public static boolean isPangram(String input) {
-    Set<Integer> set = new HashSet<>();
+    public static boolean isPangram(String input) {
+        Set<Integer> set = new HashSet<>();
 
-    input
-        .toLowerCase()
-        .chars()
-        .filter(character -> (int)'a' <= character && character <= (int)'z')
-        .forEach(
-            character -> {
-              if (!set.contains(character)) {
-                set.add(character);
-              }
-            });
-    return set.size() == 26;
-  }
+        input.toLowerCase().chars()
+                .filter(Character::isAlphabetic)
+                .forEach(character -> {
+                    if (!set.contains(character)) {
+                        set.add(character);
+                    }
+                });
+        return set.size() == 26;
+    }
+
 }
--- java/pangram/src/test/java/PangramCheckerTest.java
+++ java/pangram/src/test/java/PangramCheckerTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PangramCheckerTest {
+
     private PangramChecker pangramChecker;
 
     @Before
@@ -13,60 +14,62 @@ public class PangramCheckerTest {
         pangramChecker = new PangramChecker();
     }
 
-
     @Test
     public void emptySentenceIsNotPangram() {
         assertFalse(pangramChecker.isPangram(""));
     }
 
+    //@Ignore("Remove to run test")
+    @Test
+    public void recognizesPerfectLowerCasePangram() {
+        assertTrue(pangramChecker.isPangram("abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    //@Ignore("Remove to run test")
     @Test
     public void pangramWithOnlyLowerCaseLettersIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void phraseMissingCharacterXIsNotPangram() {
         assertFalse(pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
-    public void anotherPhraseMissingCharacterXIsNotPangram() {
-        assertFalse(pangramChecker.isPangram("the quick brown fish jumps over the lazy dog"));
+    public void phraseMissingAnotherCharacterIsNotPangram() {
+        assertFalse(pangramChecker.isPangram("five boxing wizards jump quickly at it"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void pangramWithUnderscoresIsRecognizedAsPangram() {
-        assertTrue(pangramChecker.isPangram("\"the_quick_brown_fox_jumps_over_the_lazy_dog\""));
+        assertTrue(pangramChecker.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void pangramWithNumbersIsRecognizedAsPangram() {
-        assertTrue(pangramChecker.isPangram("\"the 1 quick brown fox jumps over the 2 lazy dogs\""));
+        assertTrue(pangramChecker.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
-        assertFalse(pangramChecker.isPangram("\"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog\""));
+        assertFalse(pangramChecker.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void pangramWithMixedCaseAndPunctuationIsRecognizedAsPangram() {
         assertTrue(pangramChecker.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
     }
 
-    @Test
-    public void pangramWithNonAsciiCharactersIsRecognizedAsPangram() {
-        assertTrue(pangramChecker.isPangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."));
-    }
-
-
-    @Test
-    public void panagramInAlphabetOtherThanAsciiIsNotRecognizedAsPangram() {
-        assertFalse(pangramChecker.isPangram("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства."));
-    }
-
+    //@Ignore("Remove to run test")
     @Test
     public void upperAndLowerCaseVersionsOfTheSameCharacterShouldNotBeCountedSeparately() {
-        assertFalse(pangramChecker.isPangram("the quick brown fox jumped over the lazy FOX"));
+        assertFalse(pangramChecker.isPangram("the quick brown fox jumps over with lazy FX"));
     }
 }
