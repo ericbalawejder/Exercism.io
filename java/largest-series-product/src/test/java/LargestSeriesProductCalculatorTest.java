aebe6bf0ed09ec1b13fd9cad2d3f8bd94f564f32
Eric Balawejder <eric.balawejder@gmail.com>
2019-06-10 20:56:10 -0400

    updated java LargestSeriesProductCalculator metadata

--- java/largest-series-product/src/test/java/LargestSeriesProductCalculatorTest.java
+++ java/largest-series-product/src/test/java/LargestSeriesProductCalculatorTest.java
@@ -1,4 +1,3 @@
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,13 +9,12 @@ public class LargestSeriesProductCalculatorTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    //@Ignore
     @Test
     public void testCorrectlyCalculatesLargestProductWhenSeriesLengthEqualsStringToSearchLength() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("29");
-        final long expectedProduct = 18;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("29");
+        long expectedProduct = 18;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -24,10 +22,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLengthTwoWithNumbersInOrder() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
-        final long expectedProduct = 72;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
+        long expectedProduct = 72;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -35,10 +33,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLengthTwoWithNumbersNotInOrder() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("576802143");
-        final long expectedProduct = 48;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("576802143");
+        long expectedProduct = 48;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -46,10 +44,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLengthThreeWithNumbersInOrder() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
-        final long expectedProduct = 504;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
+        long expectedProduct = 504;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -57,10 +55,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLengthThreeWithNumbersNotInOrder() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("1027839564");
-        final long expectedProduct = 270;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("1027839564");
+        long expectedProduct = 270;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -68,10 +66,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLengthFiveWithNumbersInOrder() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
-        final long expectedProduct = 15120;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0123456789");
+        long expectedProduct = 15120;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(5);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(5);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -79,12 +77,12 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductInLongStringToSearchV1() {
-        final LargestSeriesProductCalculator calculator
+        LargestSeriesProductCalculator calculator
                 = new LargestSeriesProductCalculator("73167176531330624919225119674426574742355349194934");
 
-        final long expectedProduct = 23520;
+        long expectedProduct = 23520;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(6);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(6);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -92,10 +90,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfZeroIfAllDigitsAreZeroes() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0000");
-        final long expectedProduct = 0;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("0000");
+        long expectedProduct = 0;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(2);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -103,10 +101,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfZeroIfAllSeriesOfGivenLengthContainZero() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("99099");
-        final long expectedProduct = 0;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("99099");
+        long expectedProduct = 0;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(3);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -114,7 +112,7 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testSeriesLengthLongerThanLengthOfStringToTestIsRejected() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("123");
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("123");
 
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage(
@@ -126,10 +124,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLength0ForEmptyStringToSearch() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("");
-        final long expectedProduct = 1;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("");
+        long expectedProduct = 1;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(0);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(0);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -137,10 +135,10 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testCorrectlyCalculatesLargestProductOfLength0ForNonEmptyStringToSearch() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("123");
-        final long expectedProduct = 1;
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("123");
+        long expectedProduct = 1;
 
-        final long actualProduct = calculator.calculateLargestProductForSeriesLength(0);
+        long actualProduct = calculator.calculateLargestProductForSeriesLength(0);
 
         assertEquals(expectedProduct, actualProduct);
     }
@@ -148,7 +146,7 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testEmptyStringToSearchAndSeriesOfNonZeroLengthIsRejected() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("");
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("");
 
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage(
@@ -169,7 +167,7 @@ public class LargestSeriesProductCalculatorTest {
     //@Ignore("Remove to run test")
     @Test
     public void testNegativeSeriesLengthIsRejected() {
-        final LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("12345");
+        LargestSeriesProductCalculator calculator = new LargestSeriesProductCalculator("12345");
 
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Series length must be non-negative.");
