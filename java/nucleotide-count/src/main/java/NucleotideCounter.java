4e772134de1caff9a8efc36736d66efe317b2c7f
Eric Balawejder <eric.balawejder@gmail.com>
2019-07-18 13:19:10 -0400

    nucleotide count, method does not create empty map

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,41 +11,30 @@ class NucleotideCounter {
         System.out.println(nucleotideCounter.strand);
         System.out.println(nucleotideCounter.nucleotideCounts());
 
-        NucleotideCounter nucleotideCounter2 = new NucleotideCounter("ACCCCGT");
+        NucleotideCounter nucleotideCounter2 = new NucleotideCounter("");
         System.out.println(nucleotideCounter2.strand);
         System.out.println(nucleotideCounter2.nucleotideCounts());
 
     }
 
     private final String strand;
-    private final Map<Character, Integer> counts;
 
     NucleotideCounter(String strand) {
         if (!strand.matches("[ACGT]*")) {
             throw new IllegalArgumentException();
         }
         this.strand = strand;
-        this.counts = createCountsMap();
     }
 
     Map<Character, Integer> nucleotideCounts() {
-        char[] nucleotides = strand.toCharArray();
-        for (Character nucleotide : nucleotides) {
-            if (counts.containsKey(nucleotide)) {
-                Integer count = counts.get(nucleotide);
-                counts.put(nucleotide, count + 1);
-            }
-        }
-        return counts;
-    }
+        Character[] nucleotides = strand.chars()
+                .mapToObj(ch -> (char) ch)
+                .toArray(Character[]::new);
 
-    private Map<Character, Integer> createCountsMap() {
-        Map<Character, Integer> counts = new HashMap<>();
-        counts.put('A', 0);
-        counts.put('C', 0);
-        counts.put('G', 0);
-        counts.put('T', 0);
-        return counts;
+        Map<Character, Integer> counts = Stream.of(nucleotides)
+                .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum));
+
+        return Collections.unmodifiableMap(counts);
     }
 
 }
