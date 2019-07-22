fa6a2603a8feb1f4506332e774d5f04557994653
Eric Balawejder <eric.balawejder@gmail.com>
2019-07-22 17:18:25 -0400

    updated meta data for java Gigasecond

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,25 +1,19 @@
-// Calculate the moment when someone has lived for 10^9 seconds.
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-class Gigasecond
-{
+class Gigasecond {
     private final LocalDateTime now;
     private final long GIGASECOND = 1_000_000_000L;
-    
-    Gigasecond(LocalDate birthDate)
-    {
+
+    Gigasecond(LocalDate birthDate) {
         this.now = birthDate.atStartOfDay();
     }
 
-    Gigasecond(LocalDateTime birthDateTime)
-    {
+    Gigasecond(LocalDateTime birthDateTime) {
         this.now = birthDateTime;
     }
 
-    LocalDateTime getDate()
-    {
-        // plusSeconds(long seconds)
+    LocalDateTime getDateTime() {
         return now.plusSeconds(GIGASECOND);
     }
 }
