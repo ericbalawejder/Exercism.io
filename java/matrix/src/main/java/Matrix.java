601f59918f1020f426246f972b5433d75bf4a377
Eric Balawejder <eric.balawejder@gmail.com>
2019-07-25 11:19:23 -0400

    updated meta data for java matrix

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,4 +1,5 @@
 public class Matrix {
+
     private int[][] matrix;
     private int rows;
     private int columns;
@@ -11,9 +12,9 @@ public class Matrix {
         matrix = new int[rows][columns];
 
         for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
+            for (int column = 0; column < columns; column++) {
                 String[] rowAsStrings = rowCount[row].split(" ");
-                matrix[row][col] = Integer.parseInt(rowAsStrings[col]);
+                matrix[row][column] = Integer.parseInt(rowAsStrings[column]);
             }
         }
     }
@@ -27,25 +28,25 @@ public class Matrix {
     }
 
     int[] getRow(int row) {
-        return matrix[row];
+        return this.matrix[row - 1].clone();
     }
 
     int[] getColumn(int col) {
         int[] column = new int[rows];
         for (int row = 0; row < rows; row++) {
-            column[row] = matrix[row][col];
+            column[row] = matrix[row][col - 1];
         }
         return column;
     }
 
-    static void print(int[] array) {
+    private static void print(int[] array) {
         for (int number : array) {
             System.out.print(number + " ");
         }
         System.out.println();
     }
 
-    static void print(String[] array) {
+    private static void print(String[] array) {
         for (String number : array) {
             System.out.print(number + " ");
         }
