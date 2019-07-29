10ece849df4962f53b4f93ae6701eeb03859cbe4
Eric Balawejder <eric.balawejder@gmail.com>
2019-07-29 14:39:26 -0400

    refactored java rotational-cipher

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,33 +1,28 @@
-// Create an implementation of the Caesar cipher.
-class RotationalCipher
-{
+import java.util.stream.Collectors;
+
+class RotationalCipher {
+
     private final int shiftKey;
 
-    RotationalCipher(int shiftKey)
-    {
+    RotationalCipher(int shiftKey) {
         this.shiftKey = shiftKey;
     }
 
-    String rotate(String data)
-    {
-        StringBuffer cipher = new StringBuffer();
-        for (int i = 0; i < data.length(); i++)
-        {
-            if (Character.isUpperCase(data.charAt(i)))
-            {
-                char character = (char)(((int)data.charAt(i) + shiftKey - (int)'A') % 26 + (int)'A');
-                cipher.append(character);
-            }
-            else if (Character.isLowerCase(data.charAt(i)))
-            {
-                char character = (char)(((int)data.charAt(i) + shiftKey - (int)'a') % 26 + (int)'a');
-                cipher.append(character);
-            }
-            else
-            {
-                cipher.append(data.charAt(i));
-            }
-        }
-        return cipher.toString();
+    String rotate(String data) {
+        return data.chars()
+                .mapToObj(x -> (char) x)
+                .map(this::rotateCharacter)
+                .map(x -> x.toString())
+                .collect(Collectors.joining());
     }
+
+    private char rotateCharacter(char character) {
+        if (!Character.isAlphabetic((int) character)) {
+            return character;
+        } else {
+            int letterCase = Character.isUpperCase(character) ? 'A' : 'a';
+            return (char) (((int) character + shiftKey - letterCase) % 26 + letterCase);
+        }
+    }
+
 }
