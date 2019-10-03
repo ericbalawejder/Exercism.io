5bf0ed835faac30322ad4854864c0de3e1182518
Eric Balawejder <eric.balawejder@gmail.com>
2019-10-03 12:53:12 -0400

    kotlin hamming

---
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,6 @@
+object Hamming {
+    fun compute(strand1: String, strand2: String): Int {
+        require(strand1.length == strand2.length) { "left and right strands must be of equal length." }
+        return strand1.zip(strand2).filter { it.first != it.second }.count()
+    }
+}
