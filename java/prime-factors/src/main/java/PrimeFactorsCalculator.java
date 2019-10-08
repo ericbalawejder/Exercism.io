aa2234517dd46bd58b54de401b0bfc7d919189f6
Eric Balawejder <eric.balawejder@gmail.com>
2019-10-07 21:38:44 -0400

    refactored java prime-factors

--- java/prime-factors/src/main/java/PrimeFactorsCalculator.java
+++ java/prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -1,39 +1,51 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.LongStream;
 
 class PrimeFactorsCalculator {
 
-    List<Long> calculatePrimeFactorsOf(long number) {
-        List<Long> factors = new ArrayList<>();
+    public List<Long> calculatePrimeFactorsOf(long number) {
+        final OptionalLong lowestPrime = LongStream.rangeClosed(2, number)
+                .filter(i -> number % i == 0)
+                .findFirst();
+
+        final List<Long> primeFactors = new ArrayList<>();
+        if (lowestPrime.isPresent()) {
+            primeFactors.add(lowestPrime.getAsLong());
+            primeFactors.addAll(calculatePrimeFactorsOf(number / lowestPrime.getAsLong()));
+        }
+        return Collections.unmodifiableList(primeFactors);
+    }
+
+    List<Long> calculatePrimeFactorsOf2(long number) {
+        List<Long> primeFactors = new ArrayList<>();
         while (number % 2 == 0) {
-            factors.add((long) 2);
+            primeFactors.add((long) 2);
             number /= 2;
         }
         for (int i = 3; i <= Math.sqrt(number); i += 2) {
             while (number % i == 0) {
-                factors.add((long) i);
+                primeFactors.add((long) i);
                 number /= i;
             }
         }
         if (number > 2) {
-            factors.add(number);
+            primeFactors.add(number);
         }
-        return Collections.unmodifiableList(factors);
+        return Collections.unmodifiableList(primeFactors);
     }
 
-    List<Long> primeFactors(long number) {
-        List<Long> factors = new ArrayList<>();
+    List<Long> calculatePrimeFactorsOf3(long number) {
+        List<Long> primeFactors = new ArrayList<>();
         for (int i = 2; i <= number / i; i++) {
             while (number % i == 0) {
-                factors.add((long) i);
+                primeFactors.add((long) i);
                 number /= i;
             }
         }
         if (number > 1) {
-            factors.add(number);
+            primeFactors.add(number);
         }
-        return Collections.unmodifiableList(factors);
+        return Collections.unmodifiableList(primeFactors);
     }
 
 }
