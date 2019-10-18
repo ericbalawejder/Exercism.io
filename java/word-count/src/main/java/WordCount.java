8974b9da777026c790ef5f416b6d13682d80c66a
Eric Balawejder <eric.balawejder@gmail.com>
2019-10-18 15:07:15 -0400

    refactored java word-count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -8,11 +8,14 @@ class WordCount {
     Map<String, Integer> phrase(String sentence) {
 
         String[] words = sentence.replaceAll("[^a-zA-Z0-9\\'\\s]|\\B'|'\\B", " ")
-                .toLowerCase().trim().split("\\s+");
+                .toLowerCase()
+                .trim()
+                .split("\\s+");
 
-        Map<String, Integer> wordCountMap = Stream.of(words)
-                .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum));
-        return Collections.unmodifiableMap(wordCountMap);
+        return Stream.of(words)
+                .collect(Collectors.collectingAndThen(
+                            Collectors.toMap(key -> key, value -> 1, Integer::sum),
+                                    Collections::unmodifiableMap));
     }
 
 }
