9836f7c1ae37a49b27f4c3515e290d00b1085d47
Eric Balawejder <eric.balawejder@gmail.com>
2019-10-18 17:03:59 -0400

    refactored java collatz-conjecture

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,18 +1,14 @@
-// Collatz Conjecture https://en.wikipedia.org/wiki/Collatz_conjecture
-class CollatzCalculator
-{
-    int computeStepCount(int n)
-    {
-        if (n < 1)
-        {
+import java.util.stream.IntStream;
+
+class CollatzCalculator {
+
+    long computeStepCount(int number) {
+        if (number < 1) {
             throw new IllegalArgumentException("Only natural numbers are allowed");
         }
-        int count = 0;
-        while (n > 1)
-        {
-            n = (n % 2 == 0 ? (n / 2) : (3 * n + 1));
-            count++;
-        }
-        return count;
+        return IntStream.iterate(number, n -> n % 2 == 0 ? n / 2 : 3 * n + 1)
+                .takeWhile(n -> n != 1)
+                .count();
     }
+
 }
