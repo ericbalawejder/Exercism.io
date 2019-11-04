4d01b02552272c58e4c40027c6af679122f52d0e
Eric Balawejder <eric.balawejder@gmail.com>
2019-11-04 00:00:25 -0500

    java isogram

---
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class IsogramChecker {
+
+    boolean isIsogram(String input) {
+        String phrase = input.trim()
+                .toLowerCase()
+                .replaceAll("[^a-z]", "");
+
+        return phrase.chars()
+                .boxed()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toSet(), Collections::unmodifiableSet))
+                .size() == phrase.length();
+
+    }
+
+    boolean isIsogramImperative(String input) {
+        char[] phrase = input.trim()
+                .toLowerCase()
+                .replaceAll("[^a-z]", "")
+                .toCharArray();
+
+        Set<Character> characters = new HashSet<>();
+        for (char letter : phrase) {
+            if (characters.contains(letter)) {
+                return false;
+            } else {
+                characters.add(letter);
+            }
+        }
+        return true;
+    }
+    
+}
