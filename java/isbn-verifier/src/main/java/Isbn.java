6b3bf8130f61bb0a14c014fc5f0a1c0d34e3dd26
Eric Balawejder <eric.balawejder@gmail.com>
2020-01-13 19:31:53 -0500

    refactored java isbn-verifier

--- java/isbn-verifier/src/main/java/Isbn.java
+++ java/isbn-verifier/src/main/java/Isbn.java
@@ -1,44 +1,34 @@
 // Check if a given ISBN-10 is valid.
-public class Isbn
-{
-    boolean isValid(String stringToVerify)
-    {
+public class Isbn {
+
+    boolean isValid(String stringToVerify) {
         String isbn = stringToVerify.replaceAll("-", "");
 
-        if (isbn.length() != 10)
-        {
+        if (isbn.length() != 10) {
             return false;
         }
 
         int sumOfProduct = 0;
         // If there exists a non digit.
-        if (!isbn.matches("\\d+"))
-        {
+        if (!isbn.matches("\\d+")) {
             // This non digit must be an "X" at the end of the string.
-            if (!isbn.matches(".*X"))
-            {
+            if (!isbn.matches(".*X")) {
                 return false;
-            }
-            else
-            {
-                for (int i = 0; i < 9; i++)
-                {
+            } else {
+                for (int i = 0; i < 9; i++) {
                     int digit = Integer.parseInt(isbn.substring(i, i + 1));
                     sumOfProduct += ((10 - i) * digit);
                 }
                 sumOfProduct += 10;
             }
             return sumOfProduct % 11 == 0;
-        }
-        // All digits.
-        else
-        {
-            for (int i = 0; i < 10; i++)
-            {
+        } else {
+            for (int i = 0; i < 10; i++) {
                 int digit = Integer.parseInt(isbn.substring(i, i + 1));
                 sumOfProduct += ((10 - i) * digit);
             }
         }
         return sumOfProduct % 11 == 0;
     }
+    
 }
--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,38 +1,31 @@
 // Check if a given ISBN-10 is valid.
-public class IsbnVerifier
-{
-    boolean isValid(String stringToVerify)
-    {
+public class IsbnVerifier {
+
+    boolean isValid(String stringToVerify) {
         String isbn = stringToVerify.replaceAll("-", "");
 
-        if (isbn.length() != 10)
-        {
+        if (isbn.length() != 10) {
             return false;
         }
 
-        try
-        {
+        try {
             int sumOfProduct = 0;
-            for (int i = 0; i < 9; i++)
-            {
+
+            for (int i = 0; i < 9; i++) {
                 int digit = Integer.parseInt(isbn.substring(i, i + 1));
                 sumOfProduct += ((10 - i) * digit);
             }
-
-            if (isbn.substring(9, 10).equals("X"))
-            {
+            if (isbn.substring(9, 10).equals("X")) {
                 sumOfProduct += 10;
-            }
-            else
-            {
+            } else {
                 sumOfProduct += Integer.parseInt(isbn.substring(9, 10));
             }
             return sumOfProduct % 11 == 0;
-        }
-        catch (NumberFormatException numberFormatException)
-        {
+
+        } catch (NumberFormatException numberFormatException) {
             // Catch invalid ISBNs that have non-numeric characters in them from parseInt().
             return false;
         }
     }
+
 }
