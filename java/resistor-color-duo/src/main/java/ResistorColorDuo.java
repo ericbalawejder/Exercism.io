2f625fe33550e87f0aa3422c7444fd2408688c30
Eric Balawejder <eric.balawejder@gmail.com>
2020-01-21 12:29:09 -0500

    java resistor-color-duo

---
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
+class ResistorColorDuo {
+
+    private static final Map<String, String> RESISTORS;
+    static {
+        Map<String, String> resistorColor = new LinkedHashMap<>();
+        resistorColor.put("black", "0");
+        resistorColor.put("brown", "1");
+        resistorColor.put("red", "2");
+        resistorColor.put("orange", "3");
+        resistorColor.put("yellow", "4");
+        resistorColor.put("green", "5");
+        resistorColor.put("blue", "6");
+        resistorColor.put("violet", "7");
+        resistorColor.put("grey", "8");
+        resistorColor.put("white", "9");
+        RESISTORS = Collections.unmodifiableMap(resistorColor);
+    }
+
+    int value(String[] colors) {
+        String value = Arrays.stream(colors)
+                .filter(s -> RESISTORS.containsKey(s))
+                .map(s -> RESISTORS.get(s))
+                .limit(2)
+                .collect(Collectors.joining());
+
+        return Integer.parseInt(value);
+    }
+
+}
