904759a8b3e1fa888c9ce644f92e8dbf33975008
Eric Balawejder <eric.balawejder@gmail.com>
2020-01-24 11:46:21 -0500

    refactored java diffie-hellman

--- java/diffie-hellman/src/main/java/DiffieHellman.java
+++ java/diffie-hellman/src/main/java/DiffieHellman.java
@@ -4,7 +4,9 @@ import java.security.SecureRandom;
 class DiffieHellman {
 
     BigInteger privateKey(BigInteger prime) {
-        return getRandomBigInteger(BigInteger.ONE, prime);
+        return new BigInteger(prime.bitLength(), new SecureRandom())
+                .mod(prime.subtract(BigInteger.ONE))
+                .add(BigInteger.ONE);
     }
 
     BigInteger publicKey(BigInteger primeA, BigInteger primeB, BigInteger privateKey) {
@@ -13,16 +15,6 @@ class DiffieHellman {
 
     BigInteger secret(BigInteger prime, BigInteger publicKey, BigInteger privateKey) {
         return publicKey.modPow(privateKey, prime);
-    }
-
-    // range = [ lowerLimit, upperLimit )
-    private BigInteger getRandomBigInteger(BigInteger lowerLimit, BigInteger upperLimit) {
-        final SecureRandom secureRandom = new SecureRandom(); //NativePRNG
-        BigInteger randomNumber;
-        do {
-            randomNumber = new BigInteger(upperLimit.bitLength(), secureRandom);
-        } while(randomNumber.compareTo(upperLimit) >= 0 || randomNumber.compareTo(lowerLimit) < 0);
-        return randomNumber;
     }
 
 }
