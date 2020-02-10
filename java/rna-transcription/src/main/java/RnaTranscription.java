558ddd795c143e3b259b8f26cef73a5447d6352f
Eric Balawejder <eric.balawejder@gmail.com>
2020-02-10 00:21:19 -0500

    refactored java rna-transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,27 +1,26 @@
-public class RnaTranscription 
-{
-    public static String transcribe(String dnaStrand) 
-    {
-        char[] dnaCharacterArray = dnaStrand.toCharArray();
-        String rnaStrand = "";
-        for (int i = 0; i < dnaCharacterArray.length; i++)
-        {
-            // append each transcribed char to String rnaStrand
-            rnaStrand += rnaTranscription(dnaCharacterArray[i]);
-        }
-        return rnaStrand;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class RnaTranscription {
+
+    private static final Map<Character, Character> RNA_TRANSCRIPTION;
+    static {
+        Map<Character, Character> dnaToRna = new HashMap<>();
+        dnaToRna.put('C', 'G');
+        dnaToRna.put('G', 'C');
+        dnaToRna.put('T', 'A');
+        dnaToRna.put('A', 'U');
+        RNA_TRANSCRIPTION = Collections.unmodifiableMap(dnaToRna);
     }
 
-    // private method for character mappings
-    private static String rnaTranscription(Character dnaStrand) 
-    {
-        switch(dnaStrand) 
-        {
-            case 'C': return "G";
-            case 'G': return "C";
-            case 'T': return "A";
-            case 'A': return "U";
-            default: return "?";
-        }
+     String transcribe(String dnaStrand) {
+        return dnaStrand.chars()
+                .mapToObj(c -> (char) c)
+                .map(c -> RNA_TRANSCRIPTION.getOrDefault(c, c))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
+
 }
