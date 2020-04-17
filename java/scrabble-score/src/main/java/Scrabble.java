3aca830a096cbb9f317db85940220c79cf0b2fdc
Eric Balawejder <eric.balawejder@gmail.com>
2020-04-17 13:36:35 -0400

    refactored java scrabble-score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,30 +1,39 @@
+import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
 
 class Scrabble {
 
     private final String word;
+    private final int score;
 
-    private static final Map<Integer, Integer> characterMap = new HashMap<>();
+    private static final Map<Integer, Integer> characterValues;
 
     static {
-        "AEIOULNRST".chars().forEach(c -> characterMap.put(c, 1));
-        "DG".chars().forEach(c -> characterMap.put(c, 2));
-        "BCMP".chars().forEach(c -> characterMap.put(c, 3));
-        "FHVWY".chars().forEach(c -> characterMap.put(c, 4));
-        "K".chars().forEach(c -> characterMap.put(c, 5));
-        "JX".chars().forEach(c -> characterMap.put(c, 8));
-        "QZ".chars().forEach(c -> characterMap.put(c, 10));
+        Map<Integer, Integer> characters = new HashMap<>();
+        "AEIOULNRST".chars().forEach(c -> characters.put(c, 1));
+        "DG".chars().forEach(c -> characters.put(c, 2));
+        "BCMP".chars().forEach(c -> characters.put(c, 3));
+        "FHVWY".chars().forEach(c -> characters.put(c, 4));
+        "K".chars().forEach(c -> characters.put(c, 5));
+        "JX".chars().forEach(c -> characters.put(c, 8));
+        "QZ".chars().forEach(c -> characters.put(c, 10));
+        characterValues = Collections.unmodifiableMap(characters);
     }
 
     Scrabble(String word) {
         this.word = word;
+        this.score = calculateScore(word);
     }
 
     int getScore() {
+        return this.score;
+    }
+
+    private int calculateScore(String word) {
         return word.chars()
                 .map(Character::toUpperCase)
-                .map(letter -> characterMap.getOrDefault(letter, 0))
+                .map(letter -> characterValues.getOrDefault(letter, 0))
                 .sum();
     }
 
