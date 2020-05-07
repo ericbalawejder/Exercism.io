d6a1fd6191f07f984ff243e0407e952878696d6f
Eric Balawejder <eric.balawejder@gmail.com>
2020-05-06 21:50:45 -0400

    refactored java armstrong-numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,22 +1,12 @@
-// Write some code to determine whether a number is an Armstrong number.
-// Armstrong number: https://en.wikipedia.org/wiki/Narcissistic_number
-class ArmstrongNumbers
-{
-    boolean isArmstrongNumber(int numberToCheck)
-    {
-        int armstrongNumber = numberToCheck;
-        int numberOfDigits = (int)(Math.log10(armstrongNumber) + 1);
-        int[] digits = new int[numberOfDigits];
-        for (int i = 0; i < numberOfDigits; i++)
-        {
-            digits[i] = armstrongNumber % 10;
-            armstrongNumber = armstrongNumber / 10;
-        }
-        int sum = 0;
-        for (int digit : digits)
-        {
-            sum += (int)Math.pow(digit, numberOfDigits);
-        }
-        return sum == numberToCheck;
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+        return String.valueOf(numberToCheck)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .map(i -> Character.getNumericValue(i))
+                .map(i -> (int) Math.pow(i, String.valueOf(numberToCheck).length()))
+                .reduce(0, Integer::sum) == numberToCheck;
     }
+
 }
--- java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
+++ java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
@@ -15,12 +15,21 @@ public class ArmstrongNumbersTest {
     }
 
     @Test
+    public void zeroIsArmstrongNumber() {
+        int input = 0;
+
+        assertTrue(armstrongNumbers.isArmstrongNumber(input));
+    }
+
+    //@Ignore("Remove to run test")
+    @Test
     public void singleDigitsAreArmstrongNumbers() {
         int input = 5;
 
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void noTwoDigitArmstrongNumbers() {
         int input = 10;
@@ -28,6 +37,7 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsArmstrongNumber() {
         int input = 153;
@@ -35,6 +45,7 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsNotArmstrongNumber() {
         int input = 100;
@@ -42,6 +53,7 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsArmstrongNumber() {
         int input = 9474;
@@ -49,6 +61,7 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsNotArmstrongNumber() {
         int input = 9475;
@@ -56,6 +69,7 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsArmstrongNumber() {
         int input = 9926315;
@@ -63,6 +77,7 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
+    //@Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsNotArmstrongNumber() {
         int input = 9926314;
