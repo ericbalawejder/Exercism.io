61fd29a685f10def4af4d2ddde48a6e13c2d1636
Eric Balawejder <eric.balawejder@gmail.com>
2020-09-30 13:02:07 -0400

    refactored isSublist method

--- java/sublist/src/main/java/RelationshipComputer.java
+++ java/sublist/src/main/java/RelationshipComputer.java
@@ -1,30 +1,23 @@
 import java.util.List;
+import java.util.stream.IntStream;
 
 class RelationshipComputer<T> {
 
     Relationship computeRelationship(final List<T> list1, final List<T> list2) {
         if (list1.equals(list2)) {
             return Relationship.EQUAL;
-        } else if (isSublist(list1, list2)) {
+        } else if (isContiguousSublist(list1, list2)) {
             return Relationship.SUBLIST;
-        } else if (isSublist(list2, list1)) {
+        } else if (isContiguousSublist(list2, list1)) {
             return Relationship.SUPERLIST;
         } else {
             return Relationship.UNEQUAL;
         }
     }
 
-    private boolean isSublist(final List<T> list1, final List<T> list2) {
-        if (list1.size() > list2.size()) {
-            return false;
-        }
-        final int contiguousSublists = list2.size() - list1.size() + 1;
-
-        for (int i = 0; i < contiguousSublists; i++) {
-            if (list2.subList(i, i + list1.size()).equals(list1)) {
-                return true;
-            }
-        }
-        return false;
+    private boolean isContiguousSublist(final List<T> list1, final List<T> list2) {
+        return IntStream.range(0, list2.size() - list1.size() + 1)
+                .mapToObj(value -> list2.subList(value, value + list1.size()))
+                .anyMatch(list1::equals);
     }
 }
