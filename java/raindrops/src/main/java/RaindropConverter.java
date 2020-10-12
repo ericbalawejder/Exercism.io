28921fbcbab7da2676e99fbe5adc2d9d226e9f4f
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-12 01:12:26 -0400

    refactored raindrops

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,20 +1,24 @@
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 class RaindropConverter {
+    private static final SortedMap<Integer, String> RAINDROPS;
 
-    String convert(int number) {
-        StringBuilder rainDrops = new StringBuilder();
+    static {
+        final SortedMap<Integer, String> drops = new TreeMap<>();
+        drops.put(3, "Pling");
+        drops.put(5, "Plang");
+        drops.put(7, "Plong");
+        RAINDROPS = Collections.unmodifiableSortedMap(drops);
+    }
 
-        if (number % 3 == 0) {
-            rainDrops.append("Pling");
-        }
-        if (number % 5 == 0) {
-            rainDrops.append("Plang");
-        }
-        if (number % 7 == 0) {
-            rainDrops.append("Plong");
-        }
-        if (rainDrops.length() == 0) {
-            rainDrops.append(number);
-        }
-        return rainDrops.toString();
+    String convert(final int number) {
+        return RAINDROPS.entrySet()
+                .stream()
+                .filter(drop -> number % drop.getKey() == 0)
+                .map(drop -> drop.getValue())
+                .reduce(String::concat)
+                .orElse(Integer.toString(number));
     }
 }
