798383fb00ea277d450e9628719e45c2ab260904
Eric Balawejder <eric.balawejder@gmail.com>
2020-11-01 22:35:06 -0500

    Refactored etl

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,18 +1,16 @@
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Etl {
-
     Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        Map<String, Integer> scrabbleScores = new HashMap<>();
-        for (Map.Entry<Integer, List<String>> entry : old.entrySet()) {
-            for (String character : entry.getValue()) {
-                scrabbleScores.put(character.toLowerCase(), entry.getKey());
-            }
-        }
-        return Collections.unmodifiableMap(scrabbleScores);
+        return old.values()
+                .stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toUnmodifiableMap(
+                        String::toLowerCase, s -> old.entrySet().stream()
+                        .filter(entry -> entry.getValue().contains(s))
+                        .map(Map.Entry::getKey)
+                        .findFirst().get()));
     }
-
 }
