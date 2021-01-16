c2e8c543d964bb9f7f3dedcedf3b8a5f1fff73a6
Eric Balawejder <eric.balawejder@gmail.com>
2021-01-15 23:46:07 -0500

    Refactored getHammingDistance() method using lazy execution

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,23 +1,33 @@
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class Hamming {
-    private String leftStrand;
-    private String rightStrand;
+    private final String leftStrand;
+    private final String rightStrand;
+    private Integer hammingDistance;
 
     Hamming(String leftStrand, String rightStrand) {
         if (leftStrand.length() != rightStrand.length()) {
-            throw new IllegalArgumentException(
-                "leftStrand and rightStrand must be of equal length.");
+            if (rightStrand.isEmpty()) {
+                throw new IllegalArgumentException("right strand must not be empty.");
+            } else if (leftStrand.isEmpty()) {
+                throw new IllegalArgumentException("left strand must not be empty.");
+            } else {
+                throw new IllegalArgumentException(
+                        "leftStrand and rightStrand must be of equal length.");
+            }
         }
         this.leftStrand = leftStrand;
         this.rightStrand = rightStrand;
     }
 
+    // Lazy computation of hammingDistance.
     int getHammingDistance() {
-        return IntStream
-                .range(0, leftStrand.length())
-                .map(i -> leftStrand.charAt(i) != rightStrand.charAt(i) ? 1 : 0)
-                .reduce(0, Integer::sum);
+        return this.hammingDistance =
+                Objects.requireNonNullElseGet(hammingDistance,
+                        () -> IntStream.range(0, leftStrand.length())
+                        .map(i -> leftStrand.charAt(i) != rightStrand.charAt(i) ? 1 : 0)
+                        .reduce(0, Integer::sum));
     }
 
 }
