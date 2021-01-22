ea3d3e4e487624927ffde62d503a118e06b72e0f
Eric Balawejder <eric.balawejder@gmail.com>
2021-01-22 16:31:25 -0500

    Refactored for lazy initialization

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -3,6 +3,7 @@ import java.util.stream.IntStream;
 class NaturalNumber {
 
     private final int n;
+    private final Classification classification;
 
     NaturalNumber(int naturalNumber) {
         if (naturalNumber <= 0) {
@@ -10,10 +11,12 @@ class NaturalNumber {
                   "You must supply a natural number (positive integer)");
         }
         this.n = naturalNumber;
+        this.classification =
+                Classification.values()[Integer.compare(computeAliquotSum(), n) + 1];
     }
 
     Classification getClassification() {
-        return Classification.values()[Integer.compare(computeAliquotSum(), n) + 1];
+        return classification;
     }
 
     private int computeAliquotSum() {
