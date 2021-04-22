4d3484dfb1dd3dfc595e022dceceea3f0f25c19f
Eric Balawejder <eric.balawejder@protonmail.com>
2021-04-21 20:35:15 -0400

    Refactored slices method

--- java/series/src/main/java/Series.java
+++ java/series/src/main/java/Series.java
@@ -20,23 +20,7 @@ class Series {
         } else {
             return IntStream.rangeClosed(0, series.length() - size)
                     .mapToObj(i -> series.substring(i, i + size))
-                    .collect(Collectors.collectingAndThen(
-                            Collectors.toList(), Collections::unmodifiableList));
-        }
-    }
-
-    List<String> slicesImperative(int size) {
-        if (size > series.length()) {
-            throw new IllegalArgumentException("Slice size is too big.");
-        } else if (size <= 0) {
-            throw new IllegalArgumentException("Slice size is too small.");
-        } else {
-            List<String> sequences = new ArrayList<>();
-            for (int i = 0; i <= series.length() - size; i++) {
-                String term = series.substring(i, i + size);
-                sequences.add(term);
-            }
-            return Collections.unmodifiableList(sequences);
+                    .collect(Collectors.toUnmodifiableList());
         }
     }
 
