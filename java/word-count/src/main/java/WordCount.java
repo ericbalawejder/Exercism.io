a3dff69a63e8c0b94182f3d3fb96529832ca524c
Eric Balawejder <eric.balawejder@protonmail.com>
2021-09-02 12:28:49 -0400

    Refactored word-count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,11 +1,26 @@
 import java.util.Collections;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class WordCount {
 
-    Map<String, Integer> phrase(String sentence) {
+    private static final Pattern WORD = Pattern.compile("\\b[\\w']+\\b");
+
+    Map<String, Integer> phrase(String phrase) {
+        final Matcher matcher = WORD.matcher(phrase.toLowerCase());
+
+        return Stream.generate(matcher::find)
+                .takeWhile(found -> found)
+                .map(word -> matcher.group())
+                .collect(Collectors.groupingBy(
+                        word -> word,
+                        Collectors.reducing(0, e -> 1, Integer::sum)));
+    }
+
+    Map<String, Integer> phrase2(String sentence) {
 
         String[] words = sentence.replaceAll("[^a-zA-Z0-9\\'\\s]|\\B'|'\\B", " ")
                 .toLowerCase()
@@ -14,8 +29,8 @@ class WordCount {
 
         return Stream.of(words)
                 .collect(Collectors.collectingAndThen(
-                            Collectors.toMap(key -> key, value -> 1, Integer::sum),
-                                    Collections::unmodifiableMap));
+                        Collectors.toMap(key -> key, value -> 1, Integer::sum),
+                        Collections::unmodifiableMap));
     }
 
 }
