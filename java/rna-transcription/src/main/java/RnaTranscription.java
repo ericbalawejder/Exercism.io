8cc03e3cff2d4d4a22e59ed38499ba1494010637
Eric Balawejder <eric.balawejder@protonmail.com>
2021-09-10 23:53:30 -0400

    Refactored rna-transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,26 +1,20 @@
-import java.util.Collections;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.stream.Collectors;
+import java.util.stream.Collector;
 
 public class RnaTranscription {
 
-    private static final Map<Character, Character> RNA_TRANSCRIPTION;
-    static {
-        Map<Character, Character> dnaToRna = new HashMap<>();
-        dnaToRna.put('C', 'G');
-        dnaToRna.put('G', 'C');
-        dnaToRna.put('T', 'A');
-        dnaToRna.put('A', 'U');
-        RNA_TRANSCRIPTION = Collections.unmodifiableMap(dnaToRna);
-    }
+    private static final Map<Character, Character> RNA_TRANSCRIPTION = Map.of(
+            'C', 'G', 'G', 'C', 'T', 'A', 'A', 'U');
 
      String transcribe(String dnaStrand) {
         return dnaStrand.chars()
                 .mapToObj(c -> (char) c)
                 .map(c -> RNA_TRANSCRIPTION.getOrDefault(c, c))
-                .map(String::valueOf)
-                .collect(Collectors.joining());
+                .collect(Collector.of(
+                        StringBuilder::new,
+                        StringBuilder::append,
+                        StringBuilder::append,
+                        StringBuilder::toString));
     }
 
 }
