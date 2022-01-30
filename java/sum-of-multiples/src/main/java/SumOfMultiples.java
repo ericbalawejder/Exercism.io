8fcc1d8f0db6d749e23f1a68451fa31d5e2e612e
Eric Balawejder <eric.balawejder@protonmail.com>
2022-01-29 21:29:00 -0500

    Refactored lambda expression.

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,28 +1,27 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class SumOfMultiples {
 
     private final int sum;
 
-    SumOfMultiples(int number, int[] set) {
-        this.sum = computeSum(number, set);
+    SumOfMultiples(int limit, int[] multiples) {
+        this.sum = computeSum(limit, multiples);
     }
 
     int getSum() {
         return this.sum;
     }
 
-    private int computeSum(int number, int[] set) {
-        return IntStream.range(0, number)
-                .filter(i -> {
-                    for (int n: set) {
-                        if ((n > 0) && (i % n == 0)) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })
+    private int computeSum(int limit, int[] multiples) {
+        return IntStream.range(0, limit)
+                .filter(i -> isAFactor(multiples, i))
                 .sum();
+    }
+
+    private boolean isAFactor(int[] multiples, int i) {
+        return Arrays.stream(multiples)
+                .anyMatch(n -> (n > 0) && (i % n == 0));
     }
 
 }
